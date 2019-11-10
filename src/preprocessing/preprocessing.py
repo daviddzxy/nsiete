f19b440046a620bdfcf5a1f6a4c7e34df992c8f1
daviddzxy <david.dzxy@gmail.com>
2019-11-10 18:55:11 +0100

    small change

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,25 +1,34 @@
 import pandas as pd
 import os
-import cv2
+import numpy as np
 import argparse
+import cv2
+import re
+
+parser = argparse.ArgumentParser("Preprocessing of images in /data/raw/.")
 
 
 def main(args):
-    os.chdir("../../")  # change directory to root directory
-    annotation_path = r"./data/annotations.csv"
-    raw_data_path = r"./data/raw/"
-    processed_data_path = r"./data/processed/"
+    if re.match(".*/src/preprocessing$", os.getcwd()):
+        os.chdir("../../")  # change directory to root directory
+    annotation_path = args.a
+    raw_data_path = args.s
+    processed_data_path = args.d
     df = pd.read_csv(annotation_path)
     for index, row in df.iterrows():
         img = cv2.imread(raw_data_path + row["filename"] + ".jpg")
         img = img[row["ymin"]:row["ymax"], row["xmin"]: row["xmax"]]  # crop image
         img = cv2.resize(img, (args.r, args.r))
-        cv2.imwrite(processed_data_path + row["filename"] + ".png", img)
+        img = img / 255
+        np.save(os.path.join(processed_data_path + str(row["id"])), img)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("Preprocessing of images in /data/raw/.")
     parser.add_argument("-r", default="128", type=int, help="Width and height of resized image")
+    parser.add_argument("-s", default="./data/raw/", type=str, help="Source directory path of images")
+    parser.add_argument("-d", default="./data/processed/", type=str, help="Destination directory path of images")
+    parser.add_argument("-a", default="./data/annotations.csv", type=str, help="Path to csv annotation")
+
     parsed_args = parser.parse_args()
     main(parsed_args)
 
